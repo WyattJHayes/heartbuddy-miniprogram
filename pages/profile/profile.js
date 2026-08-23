8ef1138eebf850e9a23c08740b1c97a68ac15180
895411690@qq.com <895411690@qq.com>
2026-08-23 22:13:34 +0800

    docs+chore: README/更新日志补至v2.4 清除记录补hb_worryBox与hb_relaxScan键

--- pages/profile/profile.js
+++ pages/profile/profile.js
@@ -484,7 +484,7 @@ Page({
               'hb_lessonCounts', 'hb_scanDoneLog', 'hb_phraseLast', 'hb_reviewAll', 'hbSafeNotes',
               'hb_eduSpanDays', 'hb_callTotal', 'hb_inputDraft',
               'hb_opsCache', 'hb_lastChatTs', 'hb_talkStreak', 'hb_triggerUse', 'ach_breathe7day', 'ach_talk_3', 'ach_talk_7', 'ach_talk_30', 'ach_talk_100',
-              'hb_smallWeek', 'hb_stDrawPool'];
+              'hb_smallWeek', 'hb_stDrawPool', 'hb_relaxScan', 'hb_worryBox'];
             keys.forEach((k) => wx.removeStorageSync(k));
             this.setData({ safePeople: '', favCount: 0, eduDone: 0 });
             wx.showToast({ title: '本机记录已清空', icon: 'success' });
